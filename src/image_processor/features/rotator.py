3b97cd2f0c57f7e5c87e0294bc3e35c493ce063b
Artur Cupelli <artur.cupelli@usp.br>
2023-07-07 21:21:12 -0300

    feat: Structure class Rotator

---
+++ src/image_processor/features/rotator.py
@@ -0,0 +1,17 @@
+class ImageRotator:
+    """
+    A class to rotate images. Can be used to check if a image needs rotation.
+    """
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_text_angle(self, image) -> float:
+        """Calculate and return the current angle of the image main text."""
+        pass
+
+    @staticmethod
+    def needs_straighten(self, image) -> bool:
+        """Verify by the current text angle if needs to be rotated"""
+        pass
